ed2aad601b7b5a2f011fa86c9ab73cc8ac8e8ecf
Tim Asp <timothy.asp@gmail.com>
2016-03-17 14:47:11 -0700

    Initial commit
    This is a script adopted from Al Friedl and made into a simple wordpress plugin
    that injects the script into the footer of the page.
    
    I made a minor tweak where if the image is already linked, it swaps the link
    with the next page link

---
+++ theia-post-slider-next-page-image.php
@@ -0,0 +1,38 @@
+<?php
+/*
+Plugin Name: Theia Post Slider Next Page Image Linker
+Plugin URI:
+Description: Adds a simple script that adds a link to each image of a slideshow that links the user to the next page.
+Author: Al Friedl (Made into a plugin by Tim Asp)
+Version: 1.0.0
+*/
+
+add_action('wp_footer', 'TpsNextPageImage::wp_footer');
+
+class TpsNextPageImage {
+   public static function wp_footer() {
+ ?>
+      <script>
+         jQuery(document).ready(function($){
+            if ($('div.theiaPostSlider_slides').length > 0) {
+               var nextSlide = $('div.theiaPostSlider_nav a[rel="next"]');
+               var slideImgs = $('div.theiaPostSlider_slides img[src*="'+window.location.hostname+'"]');
+               if (nextSlide.length && slideImgs.length) {
+                  var nextLink = nextSlide.attr('href');
+                  slideImgs.each(function() {
+                     var $this = $(this);
+                     var parentEl = $this.parent()
+                     if (parentEl[0].tagName.toLowerCase() === 'a') {
+                        parentEl.attr('href', nextLink);
+                     } else {
+                        $this.wrap('<a href="' + nextLink + '"></a>');
+                     }
+                  });
+               }
+            }
+         });
+      </script>
+<?php
+   }
+}
+
